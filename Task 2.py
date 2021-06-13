b9a7e4bc938d9bf5dd782047ee3c653dbb91b58b
Srihitha2782 <85793086+Srihitha2782@users.noreply.github.com>
2021-06-13 15:50:27 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,57 @@
+# DAY - 2 BEST ENLIST BOOTCAMP
+# PRINTING A VALUE:
+
+
+print ("30 days 30 hour challenge")
+print ("30 days 30 hour challenge")
+
+#ASSIGNING STRING TO VARIABLE :
+
+hours = "thirty"
+print (hours)
+
+#STRING INDEXING
+
+Days = "Thirty Days"
+print(Days[0])
+
+challenge = "I will win"
+print(challenge[7:10])
+
+#LENGTH OF CHARACTER:
+print(len(challenge))
+
+#LOWER CHARACTER:
+print(challenge.lower())
+
+#STRING CONCATENATION:
+a = "30 Days"
+b = "30 Hours"
+c = a + b
+print(c)
+
+#ADDING SPACE DURING CONCATENATION:
+d = a + " " + b
+print(c)
+
+#CASEFOLD:
+
+text = "Thirty days thirty hours"
+x = text.casefold()
+
+print(x)
+
+#---------
+sampleText = "DON'T TROUBLE TROUBLE UNTIL TROUBLE TROUBLES YOU."
+
+y = sampleText.capitalize()
+print(y)
+
+y = sampleText.find("0")
+print(y)
+
+y = sampleText.isalpha()
+print(y)
+
+y = sampleText.isalnum()
+print(y)
